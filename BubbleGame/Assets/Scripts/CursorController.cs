84e9e09641173938b54b1d6ed8752f94d0bedacc
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-01-24 20:24:25 -0500

    Cursor's movement works well now

--- BubbleGame/Assets/Scripts/CursorController.cs
+++ BubbleGame/Assets/Scripts/CursorController.cs
@@ -2,21 +2,22 @@ using UnityEngine;
 
 public class CursorController : MonoBehaviour
 {
+    public float speed = 0.1f;
+    public Camera cam;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxis("Mouse X") < 0)
-        {
-            Debug.Log("Mouse moved left");
-        } else if (Input.GetAxis("Mouse X") > 0)
-        {
-            Debug.Log("Mouse moved right");
-        }
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 camPos = Camera.main.ScreenToWorldPoint(mousePos);
+        camPos.x = Mathf.Clamp(camPos.x, -2.5f, 2.5f);
+        transform.position = new Vector3(camPos.x, transform.position.y, camPos.z);
     }
 }
