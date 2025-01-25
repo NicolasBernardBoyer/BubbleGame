385598a302adc016b314bd00179986e01f768457
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-01-25 18:25:42 -0500

    Pearl dropping is properly randomized now

--- BubbleGame/Assets/Scripts/CursorController.cs
+++ BubbleGame/Assets/Scripts/CursorController.cs
@@ -4,14 +4,20 @@ using UnityEngine.Rendering;
 
 public class CursorController : MonoBehaviour
 {
-    public GameObject pearl;
+    public GameObject pearl1;
+    public GameObject pearl2;
+    public GameObject pearl3;
+    public GameObject pearl4;
+    bool holdingPearl = true;
+    public GameObject currentPearl;
     public float placeDelay = 0.0f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        currentPearl = Instantiate(pearl1, new Vector3(transform.position.x + Random.Range(-0.01f, 0.01f), transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
 
     // Update is called once per frame
@@ -26,12 +32,42 @@ public class CursorController : MonoBehaviour
         camPos.x = Mathf.Clamp(camPos.x, -2.5f, 2.5f);
         transform.position = new Vector3(camPos.x, transform.position.y, camPos.z);
 
+        if (holdingPearl)
+        {
+            currentPearl.transform.position = transform.position;
+        }
+
         //Allow the player to place a pearl only if the delay between placing is over
         if (Input.GetButtonDown("Fire1") && placeDelay <= 0)
         {
-            //create a new pearl and reset the timer
-            Instantiate(pearl, new Vector3(transform.position.x + Random.Range(-0.01f,0.01f), transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+            currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            holdingPearl = false;
             placeDelay = 1.0f;
+        }
+
+        if (placeDelay <= 0 && holdingPearl == false)
+        {
+            int pearlNum = RandomizePearl();
+            GameObject pearlToUse = pearl1;
+            switch (pearlNum)
+            {
+                case 1:
+                    pearlToUse = pearl1;
+                    break;
+                case 2:
+                    pearlToUse = pearl2;
+                    break;
+                case 3:
+                    pearlToUse = pearl3;
+                    break;
+                case 4:
+                    pearlToUse = pearl4;
+                    break;
+            }
+            //create a new pearl and reset the timer
+            currentPearl = Instantiate(pearlToUse, new Vector3(transform.position.x + Random.Range(-0.01f,0.01f), transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+            currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            holdingPearl = true;
         }
     }
 
