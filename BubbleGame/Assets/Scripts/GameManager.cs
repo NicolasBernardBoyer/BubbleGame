26c3abeb1946c5cb89c5e416e1dafa71c7eb620e
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-01-25 21:01:50 -0500

    added ice tag

--- BubbleGame/Assets/Scripts/GameManager.cs
+++ BubbleGame/Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ public class GameManager : MonoBehaviour
 {
     public int pearlsColliding;
     public GameObject cursor;
+    public GameObject iceCube;
+    public int moveCount = 0;
     public float iceDelay = 30.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,9 +17,16 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //check for ice timer, create the ice if it is up
+        IceTimer();
+        if (iceDelay <= 0)
+        {
+            createIce();
+            iceDelay = 30.0f;
+        }
     }
 
+    //check for specifically two objects colliding, if so instantiate a new one
     public void CreatePearl(GameObject pearl, Vector3 pos)
     {
         if (pearlsColliding == 2)
@@ -27,6 +36,17 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //spawn ice cubes X times
+    private void createIce()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(iceCube, new Vector3(Random.Range(-2.9f, 3.0f),
+            cursor.transform.position.y-5, cursor.transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        }
+    }
+
+    //timer that determines when ice is dropped
     private void IceTimer()
     {
         if (iceDelay > 0)
