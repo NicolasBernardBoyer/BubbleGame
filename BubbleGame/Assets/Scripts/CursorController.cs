7735269c6af4308b8abee5af2af93dd90c24b2cc
NicolassBoyer <nicolasbernardboyer@gmail.com>
2025-01-26 15:45:55 -0500

    Lose screen active

--- BubbleGame/Assets/Scripts/CursorController.cs
+++ BubbleGame/Assets/Scripts/CursorController.cs
@@ -17,64 +17,67 @@ public class CursorController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentPearl = Instantiate(pearl1, new Vector3(transform.position.x + Random.Range(-0.01f, 0.01f), transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+        currentPearl = Instantiate(pearl1, new Vector3(transform.position.x + Random.Range(-0.01f, 0.01f), transform.position.y, 100), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
         currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CountDelay();
-
-        //Get the mouse position, and move the cursor's x position according to the mouse's position
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = Camera.main.nearClipPlane;
-        Vector3 camPos = Camera.main.ScreenToWorldPoint(mousePos);
-        camPos.x = Mathf.Clamp(camPos.x, -2.5f, 2.5f);
-        transform.position = new Vector3(camPos.x, transform.position.y, camPos.z);
-
-        if (holdingPearl)
+        if (FindFirstObjectByType<GameManager>().lose == false)
         {
-            currentPearl.transform.position = transform.position;
-        }
+            CountDelay();
 
-        //Allow the player to place a pearl only if the delay between placing is over
-        if (Input.GetButtonDown("Fire1") && placeDelay <= 0)
-        {
-            currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            //increase move count only if ice is present on the board
-            IceCube[] iceCubes = FindObjectsByType<IceCube>(FindObjectsSortMode.None);
-            foreach (IceCube cube in iceCubes)
+            //Get the mouse position, and move the cursor's x position according to the mouse's position
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+            Vector3 camPos = Camera.main.ScreenToWorldPoint(mousePos);
+            camPos.x = Mathf.Clamp(camPos.x, -2.5f, 2.5f);
+            transform.position = new Vector3(camPos.x, transform.position.y, camPos.z);
+
+            if (holdingPearl)
             {
-                cube.movesLeft--;
+                currentPearl.transform.position = transform.position;
             }
-            holdingPearl = false;
-            placeDelay = 1.0f;
-        }
 
-        if (placeDelay <= 0 && holdingPearl == false)
-        {
-            int pearlNum = RandomizePearl();
-            GameObject pearlToUse = pearl1;
-            switch (pearlNum)
+            //Allow the player to place a pearl only if the delay between placing is over
+            if (Input.GetButtonDown("Fire1") && placeDelay <= 0)
             {
-                case 1:
-                    pearlToUse = pearl1;
-                    break;
-                case 2:
-                    pearlToUse = pearl2;
-                    break;
-                case 3:
-                    pearlToUse = pearl3;
-                    break;
-                case 4:
-                    pearlToUse = pearl4;
-                    break;
+                currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                //increase move count only if ice is present on the board
+                IceCube[] iceCubes = FindObjectsByType<IceCube>(FindObjectsSortMode.None);
+                foreach (IceCube cube in iceCubes)
+                {
+                    cube.movesLeft--;
+                }
+                holdingPearl = false;
+                placeDelay = 1.0f;
             }
-            //create a new pearl and reset the timer
-            currentPearl = Instantiate(pearlToUse, new Vector3(transform.position.x + Random.Range(-0.01f,0.01f), transform.position.y, transform.position.z), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
-            currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-            holdingPearl = true;
+
+            if (placeDelay <= 0 && holdingPearl == false)
+            {
+                int pearlNum = RandomizePearl();
+                GameObject pearlToUse = pearl1;
+                switch (pearlNum)
+                {
+                    case 1:
+                        pearlToUse = pearl1;
+                        break;
+                    case 2:
+                        pearlToUse = pearl2;
+                        break;
+                    case 3:
+                        pearlToUse = pearl3;
+                        break;
+                    case 4:
+                        pearlToUse = pearl4;
+                        break;
+                }
+                //create a new pearl and reset the timer
+                currentPearl = Instantiate(pearlToUse, new Vector3(transform.position.x + Random.Range(-0.01f, 0.01f), transform.position.y, 100), Quaternion.Euler(new Vector3(0, 0, Random.Range(0, 360))));
+                currentPearl.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                holdingPearl = true;
+            }
         }
     }
 
